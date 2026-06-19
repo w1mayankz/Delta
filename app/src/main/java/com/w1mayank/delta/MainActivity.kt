7b9540ee16818a68d2ff7e4071a8d5512316ec9b
Mayank <w1mayankz@gmail.com>
2026-06-19 21:39:12 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/w1mayank/delta/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.w1mayank.delta
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // This is the Jetpack Compose engine taking over the screen
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color.Black
+            ) {
+                Text(
+                    text = "Delta Engine Ready.", 
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
