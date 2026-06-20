10aa9d34ce43966d37315817cfcb1ea2143db986
Mayank <w1mayankz@gmail.com>
2026-06-20 10:45:27 +0530

    Create Type.kt

---
+++ app/src/main/java/com/w1mayank/delta/ui/theme/Type.kt
@@ -0,0 +1,13 @@
+package com.w1mayank.delta.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.w1mayank.delta.R
+
+val InterFont = FontFamily(
+    Font(R.font.inter_18pt_regular, FontWeight.Normal),
+    Font(R.font.inter_18pt_medium, FontWeight.Medium),
+    Font(R.font.inter_18pt_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_18pt_bold, FontWeight.Bold)
+)
